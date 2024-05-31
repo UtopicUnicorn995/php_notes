485fc6f93eb1403f1cc130fc5ed349a17141aa49
utopicUnicorn <christianbarrios995@gmail.com>
2024-05-31 16:19:09 +0800

    php-notes: additional php codes for inserting data to database

--- section_12_databases/includes/formhandler.inc.php
+++ section_12_databases/includes/formhandler.inc.php
@@ -1,16 +1,39 @@
 <?php
 
-if($_SERVER["REQUEST_METHOD"] == 'POST'){
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
     $username = $_POST["username"];
     $pdw = $_POST["passwords"];
     $email = $_POST["email"];
+    // Don't have to sanitize data unless we are outputing data in the frontend 
+    try {
+        // require_once when there is a script we want to use
+        require_once 'dbh.inc.php';
+        // require
+        // include
+        // include_once
+        // the 3 requre and include above does the same thing but with a slight variation
 
-    try{
+        $query = "INSERT INTO users (username, pwd, email) VALUES 
+        (?, ?, ?);";
 
-    }catch(PDOException $e){
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([$username, $pwd, $email]);
+
+        $pdo = null;
+        $stmt = null;
+
+        header("Location: ../index.php");
+
+        // close a script without any database running
+        //exit();
+
+        // when there is a connection on it.
+        die();
+
+    } catch (PDOException $e) {
+        // die() -> terminates the script
         die('Query failed: "' . $e->getMessage());
     }
-
-}else{
+} else {
     header("Location: ../index.php");
 }
