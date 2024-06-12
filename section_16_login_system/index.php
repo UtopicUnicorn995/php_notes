0854053f51e3dcc01e4b09f2bf6cbea911f72e86
utopicUnicorn <christianbarrios995@gmail.com>
2024-06-12 14:37:58 +0800

    php-notes: added some includes files

---
+++ section_16_login_system/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <h3>Login</h3>
+
+    <form action="includes/login.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pdw" placeholder="Password">
+        <button>Login</button>
+    </form>
+
+    
+    <h3>Signup</h3>
+
+    <form action="includes/signup.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pdw" placeholder="Password">
+        <input type="password" name="email" placeholder="E-Mail">
+        <button>Signup</button>
+    </form>
+
+</body>
+</html>
