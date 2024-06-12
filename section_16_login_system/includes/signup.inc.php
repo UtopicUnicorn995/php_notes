3250332659b8e60f92abaa068d6b35ce721b099a
utopicUnicorn <christianbarrios995@gmail.com>
2024-06-12 18:43:35 +0800

    php-notes: added some files for signing up

---
+++ section_16_login_system/includes/signup.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $username = $_POST['username'];
+    $pwd = $_POST['password'];
+    $email = $_POST['email'];
+
+    try {
+        require_once 'dbh.inc.php';
+    } catch (PDOException $e) {
+        die('Query failed: ' . $e->getMessage());
+    }
+} else {
+    header('Location: ../index.php');
+    die();
+}
